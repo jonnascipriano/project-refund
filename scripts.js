ffef7a08cd4eca7dfe41325a7d9b5a80f18da063
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-02-18 21:39:39 -0300

    removendo um item da lista

--- scripts.js
+++ scripts.js
@@ -158,3 +158,18 @@ function updateTotal(){
         alert("Não foi possível atualizar os totais")
     }
 }
+
+// Evento que captura o clique nos itens da lista.
+expenseList.addEventListener("click", function(event){
+    // Verifica se o elemento clicado é o ícone de remover.
+    if(event.target.classList.contains('remove-icon')){
+        // Obtém a li pai do elemento clicado.
+        const item = event.target.closest(".expense")
+
+
+
+        // Remove o item da lista.
+        item.remove()
+    }
+    updateTotal()
+})
